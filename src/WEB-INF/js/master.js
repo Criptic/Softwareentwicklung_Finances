78e0dab568e79762e7b8a9671ad606656c01882d
Marvin Messenzehl <marvin.messenzehl@gmx.de>
2016-04-12 10:55:16 +0200

    Addding papaparse for parsing csv data. Implementing some functions in javascript for testing Colouring

--- src/WEB-INF/js/master.js
+++ src/WEB-INF/js/master.js
@@ -1,3 +1,27 @@
 function showApple(){
   alert('clicked');
 }
+
+//Dummy load Data from papaparse
+function loadFile(){
+  Papa.parse("../../src/main/resources/dummy.csv", {
+ 	download: true,
+ 	complete: function(results) {
+ 		console.log(results);
+ 	}
+ });
+}
+
+//Dummy function to change Color of Thumbnail
+function changeColorRed(){
+  d3.select("#apple-logo").style("background-color", "#F44336");
+}
+
+function changeColorYellow(){
+  d3.select("#twitter-logo").style("background-color", "#FFC107");
+}
+
+
+function changeColorGreen(){
+  d3.select("#facebook-logo").style("background-color", "#388E3C");
+}
