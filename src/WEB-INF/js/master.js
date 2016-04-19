e89f23fd7bbdaa2853f7bbf543a5fc6dd8c9a037
Marvin Messenzehl <marvin.messenzehl@gmx.de>
2016-04-19 11:34:06 +0200

    Implementing javascript logic for getting local csv data just from browser

--- src/WEB-INF/js/master.js
+++ src/WEB-INF/js/master.js
@@ -1,16 +1,41 @@
-function showApple(){
-  alert('clicked');
+//Defining Variables for global use
+var results = [];
+
+
+//jQuery pre processor for handling a file in browser on client side
+$.ajaxPrefilter( 'script', function( options ) {
+    options.crossDomain = true;
+});
+
+//Get Data from definded url. In this case it is a path
+$.ajax({
+    type: "GET",
+    url: "../main/resources/dummy.csv",
+    dataType: "text",
+    success: function(data) {
+        console.log(data);
+        console.log($.type(data));
+        parseString(data);
+    },
+    error: function (request, status, error) {
+
+        alert(error);
+    }
+ });
+
+//Papaparse function for casting csv string into array.
+//Function is called in success case as callback function
+function parseString(string){
+  results = Papa.parse(string);
+  //show the data in console as array
+  console.log(results.data);
+  //Give value to the global variable
+  results = results.data;
 }
 
-//Dummy load Data from papaparse
-function loadFile(){
-  Papa.parse("../../src/main/resources/dummy.csv", {
- 	download: true,
- 	complete: function(results) {
- 		console.log(results);
- 	}
- });
-}
+
+
+////////////////////////////////////////////////
 
 //Dummy function to change Color of Thumbnail
 function changeColorRed(){
